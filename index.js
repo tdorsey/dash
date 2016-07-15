e9ba900ed508d4dca0d0e73a97cabf309ffe9e23
Tommy Dorsey <tdorsey@gmail.com>
2016-07-15 08:37:42 -0400

    print debug on button press

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-
-var dash_button  = require('dash-button');
-
-//import DashButton from 'dash-button';
+var DashButton = require('dash-button');
 
 const DASH_BUTTON_MAC_ADDRESS = 'A0:63:91:D6:82:33';
+let button = new DashButton(DASH_BUTTON_MAC_ADDRESS);
 
-var button = new dash_button.DashButton(DASH_BUTTON_MAC_ADDRESS);
+var subscription = button.addListener(function(){
+	process.stdout.write("hello: ");
 
-//let button = new DashButton(DASH_BUTTON_MAC_ADDRESS);
-debugger;
+});//do stuff
 
-var subscription = button.addListener(async () => {
-alert("hi");//do stuff
-});
+//subscription.remove();
 
-subscription.remove();
+
+
