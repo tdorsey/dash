c7f9007f158ecb0302f167bed4b365570ae8fb01
Tommy Dorsey <tdorsey@gmail.com>
2016-07-15 09:38:18 -0400

    attempt loading harmony api

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var dash_button = require('node-dash-button');
+var harmony = require('orchestra-jsapi');
 var dash = dash_button("A0:63:91:D6:82:33"); //address from step above
 dash.on("detected", function (){
 debugger;
-    console.log("omg found");
+    console.log("button pressed");
+    harmony.getDiscoveredHubs();	
 });
 
 
