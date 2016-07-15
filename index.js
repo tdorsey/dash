12cd50f7155b3ac093789ad1042d55b69516a052
tdorsey <tdorsey@vestapartners.com>
2016-07-15 10:08:47 -0400

    change harmony reference

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var dash_button = require('node-dash-button');
-var harmony = require('orchestra-jsapi');
+var harmonyLib = require('orchestra-jsapi');
+var harmony = new harmonyLib();
 var dash = dash_button("A0:63:91:D6:82:33"); //address from step above
 dash.on("detected", function (){
-debugger;
     console.log("button pressed");
     harmony.getDiscoveredHubs();	
 });
